eadeb7822a3f5aaf9f1ff79a9f034e7a148a12ef
sinopeSA <shubhamagarawal10@gmail.com>
2020-06-09 12:20:10 +0530

    added code

---
+++ code.js
@@ -0,0 +1,26 @@
+let config = {
+     width : 800,
+     height :600,
+     scene :{
+        Preload: preload,
+        Create: create,
+        Update: update
+
+     }
+
+
+};
+
+let game = new phaser.Game(config);
+
+function preload(){
+
+}
+
+function create(){
+
+}
+
+function update(){
+
+}
